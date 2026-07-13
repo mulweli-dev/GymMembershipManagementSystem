41766d090fca85eeac9615d04111fba3b584e5bb
mulwelicodes <mulweliprogressramugondo@gmail.com>
2026-07-13 23:38:57 +0200

    Add member attributes to Member class

--- Member.java
+++ Member.java
@@ -1,3 +1,10 @@
 public class Member {
     
+private int memberID;
+private String fullName;
+private int age;
+private String gender;
+private String phoneNumber;
+private String membershipPlan;
+
 }
